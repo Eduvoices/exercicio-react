b266c79edef575812ac214713bbdb73547f1085c
Luis Eduardo <eduvoices25@gmail.com>
2023-03-30 12:02:34 -0300

    fazendo correções

--- src/App.jsx
+++ src/App.jsx
@@ -11,54 +11,6 @@ function App() {
       (peso / (altura * altura)).toFixed(1)
     )
   }
-  
-
-  // const printResultado = () => {
-    
-  //   if (calcImc() < 18.5) {
-  //     return (
-  //       <>
-  //         <td>Menor que 18,5</td>
-  //         <td>Abaixo do peso ideal (Magreza)</td>
-  //       </>
-  //     )
-  //   } else if (calcImc() <= 24.9) {
-  //     return (
-  //       <>
-  //         <td>Entre 18,5 e 24,9</td>
-  //         <td>Peso normal</td>
-  //       </>
-  //     )
-  //   } else if (calcImc() <= 29.9) {
-  //     return (
-  //       <>
-  //         <td>Entre 25 e 29,9</td>
-  //         <td>Sobrepeso</td>
-  //       </>
-  //     )
-  //   } else if (calcImc() <= 34.9) {
-  //     return (
-  //       <>
-  //         <td>Entre 30 e 34,9</td>
-  //         <td>Obesidade (Grau I)</td>
-  //       </>
-  //     )
-  //   } else if (calcImc() <= 39.9) {
-  //     return (
-  //       <>
-  //         <td>Entre 35 e 39,9</td>
-  //         <td>Obesidade (Grau II)</td>
-  //       </>
-  //     )
-  //   } else if (calcImc() > 40) {
-  //     return (
-  //       <>
-  //         <td>Maior que 40</td>
-  //         <td>Obesidade (Grau III)</td>
-  //       </>
-  //     )
-  //   }
-  // }
 
 const mensagem = () => {
   if (calcImc() > 1) {
@@ -74,23 +26,13 @@ const mensagem = () => {
       <h1>Calculadora de IMC - Índice de Massa Corpórea</h1>
 
       <form>
-        <input type="number" placeholder='Digite seu peso' onChange={e => setPeso(e.target.value)} />
-        <input type="number" placeholder='Digite sua altura' onChange={e => setAltura(e.target.value)} />
+        <input type="number" placeholder='Digite seu peso em kg (Ex.: 80)' onChange={e => setPeso(e.target.value)} />
+        <input type="number" placeholder='Digite sua altura em metros (Ex.: 1,80)' onChange={e => setAltura(e.target.value)} />
       </form>
 
       <h3>{mensagem()}</h3>
 
       <Tabela resultado={calcImc()}/>
-
-      {/* <table className='table-area'>
-        <thead>
-          <th>IMC</th>
-          <th>Classificação</th>
-        </thead>
-        <tr>
-          {printResultado()}
-        </tr>
-      </table> */}
     </div>
   )
 }
